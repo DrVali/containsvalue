700ef8f8ffe90c1d30c22aa65b7defd7bd6abcca
DrVali <ahmadi.world@gmail.com>
2024-09-14 08:39:59 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
    "use strict";
 
 
-
-
-
 var myArray = [ 'ali', 'hasan', 'abbas', 'adel', 'alireza' ];
 var value ='hasan'   ;
 containsValue(myArray,value);
@@ -12,28 +9,8 @@ function containsValue(myArray, value) {
 
 
     if(myArray.includes(value)) {
-    alert(  " The array contains the value " + value  + "  at index" + myArray.indexof(value)) ;
-
-
-
+    alert(  " The array contains the value " + value  + "  at index" + myArray.indexOf(value)) ;
   }
-
 }
 
-   /*
-function containsValue(arr,value){
-
-    for( i=0;i<arr.lenght;i++){
-      if(arr[0]=== value){
-               console.log("ali");
-
-
-      }
-      else alert (`The array does not contain the value ${value}.`);
-    }
-
-}
-
- containsValue(arrayList, valueSerch) ;
-
- */
+  
